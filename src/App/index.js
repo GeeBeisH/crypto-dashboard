54e53fbba46a11d17ce6fbf4ca5a892d009b0878
geebeish <geebeish@mweb.co.za>
2020-05-15 16:22:08 +0200

    Add App Layout

--- src/App/index.js
+++ src/App/index.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import WelcomeMessage from './WelcomeMessage';
 import './App.css';
-import styled, { css } from 'styled-components';
-
-const MyButton = styled.div`
-	color: green;
-	${(props) => props.primary && css`color: palevioletred;`};
-`;
-
-const TomatoButton = styled(MyButton)`
-color: tomato;
-border-color: tomato;
-`;
+import AppLayout from './AppLayout';
 
 function App() {
 	return (
-		<div className="App">
+		<AppLayout>
 			<WelcomeMessage />
-			<MyButton>Hello</MyButton>
-			<MyButton primary>Hello</MyButton>
-			<TomatoButton>Hello</TomatoButton>
-		</div>
+		</AppLayout>
 	);
 }
 
