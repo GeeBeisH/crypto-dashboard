5d11acf7b551936db4bbc9b8f5b4cf2b9ed53688
geebeish <geebeish@mweb.co.za>
2020-05-15 18:40:19 +0200

    Add AppBar and grid styling

--- src/App/index.js
+++ src/App/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import WelcomeMessage from './WelcomeMessage';
 import './App.css';
 import AppLayout from './AppLayout';
+import AppBar from './AppBar';
 
 function App() {
 	return (
 		<AppLayout>
+			<AppBar />
 			<WelcomeMessage />
 		</AppLayout>
 	);
