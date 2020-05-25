41abecd7efa22b3ae4fe5ec4e5fbf9a2ffd77309
geebeish <geebeish@mweb.co.za>
2020-05-25 14:44:06 +0200

    Add ChartSelect

---
+++ src/Dashboard/ChartSelect.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { backgroundColor2, fontSize2 } from '../Shared/Styles';
+
+export default styled.select`
+	color: #008080;
+	border: 1px solid;
+	margin: 5px;
+	float: right;
+	cursor: pointer;
+	${backgroundColor2} ${fontSize2};
+`;
